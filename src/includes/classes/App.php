5402bfa1a7c9932b5235ec37718bb4cd74e02047
jaswsinc <jaswsinc@wsharks.com>
2016-05-04 00:01:09 -0800

    Updating skeleton base config.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -52,8 +52,104 @@ class App extends SCoreClasses\App
                 ],
             ],
 
+            '©brand' => [
+                '©text_domain' => '',
+                '©slug'        => '',
+                '©var'         => '',
+                '©name'        => '',
+                '©acronym'     => '',
+                '©prefix'      => '',
+
+                '§domain'      => '',
+                '§domain_path' => '',
+            ],
+
             '§default_options' => [],
             '§pro_option_keys' => [],
+
+            '§conflicts' => [
+                '§plugins' => [
+                    /*
+                        '[slug]'  => '[name]',
+                    */
+                ],
+                '§themes' => [
+                    /*
+                        '[slug]'  => '[name]',
+                    */
+                ],
+                '§deactivatable_plugins' => [
+                    /*
+                        '[slug]'  => '[name]',
+                    */
+                ],
+            ],
+            '§dependencies' => [
+                '§plugins' => [
+                    /*
+                        '[slug]' => [
+                            'name'        => '',
+                            'url'         => '',
+                            'archive_url' => '',
+                            'in_wp'       => true,
+                            'test'        => function(string $slug) {},
+
+                            A test function is optional.
+                            A successful test must return `true`.
+                            A failed test must return an array with:
+                                - `reason`      = One of: `needs-upgrade|needs-downgrade`.
+                                - `min_version` = Min version, if `reason=needs-upgrade`.
+                                - `max_version` = Max version, if `reason=needs-downgrade`.
+                        ],
+                    */
+                ],
+                '§themes' => [
+                    /*
+                        '[slug]' => [
+                            'name'        => '',
+                            'url'         => '',
+                            'archive_url' => '',
+                            'in_wp'       => true,
+                            'test'        => function(string $slug) {},
+
+                            A test function is optional.
+                            A successful test must return `true`.
+                            A failed test must return an array with:
+                                - `reason`      = One of: `needs-upgrade|needs-downgrade`.
+                                - `min_version` = Min version, if `reason=needs-upgrade`.
+                                - `max_version` = Max version, if `reason=needs-downgrade`.
+                        ],
+                    */
+                ],
+                '§others' => [
+                    /*
+                        '[arbitrary key]' => [
+                            'name'        => '', // Short plain-text name; i.e., '[name]' Required
+                            'description' => '', // Brief rich-text description; i.e., It requires [description].
+                            'test'        => function(string $key) {},
+
+                            A test function is required.
+                            A successful test must return `true`.
+                            A failed test must return an array with:
+                                - `how_to_resolve` = Brief rich-text description; i.e., → To resolve, [how_to_resolve].
+                                - `cap_to_resolve` = Cap required to satisfy; e.g., `manage_options`.
+                        ],
+                    */
+                ],
+            ],
+
+            '§notices' => [
+                '§on_install' => function (array $installion_history) {
+                    return [
+                        'is_transient' => true,
+                        'markup'       => sprintf(
+                            __('<strong>%1$s</strong> v%2$s installed successfully.', 'wp-sharks-core'),
+                            esc_html($this->Config->©brand['©name']),
+                            esc_html($this->c::version())
+                        ),
+                    ];
+                },
+            ],
         ];
         parent::__construct($instance_base, $instance);
     }
